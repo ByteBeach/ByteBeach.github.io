8f9ca87ad3c0c7cf389588f9b604316c443bf752
ByteBeach <Stef.Popp@outlook.com>
2023-09-22 16:36:42 -0700

    fix url to find css and js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/<ByteBeach.github.io>' : '',
+    assetPrefix: isProd ? '/<ByteBeach.github.io>' : ''
 }
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = nextConfig
 
